76d4cde5672d6e23fb342cfcdfd2b1b1830b417f
mymuna-rahman <mymunarahman03@gmail.com>
2024-07-18 01:00:50 +0600

    Created FXML scenes for Patrol Routes and Schedules.
    Scenes for User: Security Guard has been completed. Some little modifications in other scenes has also been done.

---
+++ NationalParkManagementApp/src/main/java/org/nationalparkmanagementapp/Users/Mymuna/SecurityGuard/Controller/changeProposalViewController.java
@@ -0,0 +1,7 @@
+package org.nationalparkmanagementapp.Users.Mymuna.SecurityGuard.Controller;
+
+public class changeProposalViewController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ NationalParkManagementApp/src/main/java/org/nationalparkmanagementapp/Users/Mymuna/SecurityGuard/Controller/patrolRouteDetailsViewController.java
@@ -0,0 +1,7 @@
+package org.nationalparkmanagementapp.Users.Mymuna.SecurityGuard.Controller;
+
+public class patrolRouteDetailsViewController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
