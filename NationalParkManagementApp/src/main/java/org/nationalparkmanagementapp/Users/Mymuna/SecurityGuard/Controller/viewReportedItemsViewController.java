5d490ff7550bf899859e02ee1ffe3d2d4f6fc4b1
mymuna-rahman <mymunarahman03@gmail.com>
2024-07-17 20:35:39 +0600

    Completed scenes for Lost and Found.

---
+++ NationalParkManagementApp/src/main/java/org/nationalparkmanagementapp/Users/Mymuna/SecurityGuard/Controller/viewReportedItemsViewController.java
@@ -0,0 +1,7 @@
+package org.nationalparkmanagementapp.Users.Mymuna.SecurityGuard.Controller;
+
+public class viewReportedItemsViewController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
